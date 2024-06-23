56583e0711624bb462ab8d338923da5f0598bfae
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-06-23 16:53:38 -0300

    feat: server

--- app/server/src/app.module.ts
+++ app/server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { DocumentTypesModule } from './document-types/document-types.module';
+import { DocumentsModule } from './documents/documents.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-    imports: [UsersModule, DatabaseModule, DocumentTypesModule],
+    imports: [UsersModule, DatabaseModule, DocumentTypesModule, DocumentsModule, TagModule],
     controllers: [],
     providers: [],
 })
--- app/server/src/users/dto/create-user.dto.ts
+++ app/server/src/users/dto/create-user.dto.ts
@@ -1,20 +1,20 @@
 import { IsEmail, IsIn, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @MinLength(1)
-  @MaxLength(100)
-  name: string;
+    @IsString()
+    @MinLength(1)
+    @MaxLength(100)
+    name: string;
 
-  @IsString()
-  @IsEmail()
-  @MaxLength(250)
-  email: string;
+    @IsString()
+    @IsEmail()
+    @MaxLength(250)
+    email: string;
 
-  @IsString()
-  password: string;
+    @IsString()
+    password: string;
 
-  @IsString()
-  @IsIn(['ADMIN', 'USER'])
-  type: 'ADMIN' | 'USER';
+    @IsString()
+    @IsIn(['ADMIN', 'USER'])
+    type: 'ADMIN' | 'USER';
 }
